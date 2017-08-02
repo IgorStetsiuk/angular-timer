a4215f0872ecd7d1cb7978450ede727d2c0a144f
Angular CLI <angular-cli@angular.io>
2017-08-02 22:28:45 +0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TimeAppPage } from './app.po';
+
+describe('time-app App', () => {
+  let page: TimeAppPage;
+
+  beforeEach(() => {
+    page = new TimeAppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
